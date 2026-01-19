8871fd8a97d075efdbb1c23fd4a6e52974f35b2a
Kavya R Menon <kavyarmenon98@gmail.com>
2026-01-19 10:14:08 +0530

    review editted5

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -4,7 +4,7 @@ const reviewSchema = new mongoose.Schema(
   {
     productId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
+      ref: "product",
       required: true,
     },
     userId: {
